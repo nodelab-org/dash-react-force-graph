84f4f48fc111f023e6d54090e1d360c693851754
Jon Thompson <jjt@tutanota.com>
2022-03-03 17:11:35 +0100

    fix webpacking of font output names

--- webpack.config.js
+++ webpack.config.js
@@ -92,7 +92,7 @@ module.exports = (env, argv) => {
                     test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
                     type: 'asset/resource',
                     generator: {
-                        filename: 'fonts/[name].[ext]'
+                        filename: 'fonts/[name][ext]'
                     }
                     // use: [
                     //     {
